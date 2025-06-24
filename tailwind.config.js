ef3f1a0c22ef96fd96adce37bc9fcdcb046578b6
Aditi Gaur <aditigaur126@gmail.com>
2025-06-24 22:18:07 +0530

    first push

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+// tailwind.config.js
+import colors from 'tailwindcss/colors';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        border: "hsl(var(--border))",
+      }
+    }
+  },
+  plugins: [],
+}
